4ce762e36e4bedc5f7dec150824e89fe215e7f91
vidriktt <vidriktoom@gmail.com>
2019-03-19 12:03:25 +0200

    update

--- src/Yatzy.java
+++ src/Yatzy.java
@@ -82,6 +82,7 @@ public class Yatzy {
                     sum += lahter;
             }
             return sum;
+
 //        } else if (lahter == 7) {
 //            for (Täring täring : täringud) {
 //                temp = täring.getArv();
